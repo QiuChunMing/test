932e8c60b28251ff096805f82526b4c2714f5669
QiuChunMing <1148082901@qq.com>
2019-01-02 23:34:55 +0800

    初次提交

---
+++ src/pages/Order/Order.vue
@@ -0,0 +1,67 @@
+<template>
+  <section class="order">
+    <section class="order_no_login">
+      <img src="./images/person.png" alt>
+      <h3>登陆后查看外卖订单</h3>
+      <button>立即登录</button>
+    </section>
+    <section class="order_no_login">暂无订单</section>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus">
+@import '../../common/stylus/mixins.styl'
+.order
+  width 100%
+  .header
+    background-color #02a774
+    position fixed
+    z-index 100
+    left 0
+    top 0
+    width 100%
+    height 45px
+    .header_search
+      position absolute
+      left 15px
+      top 50%
+      transform translateY(-50%)
+      width 10%
+      height 50%
+      .iconfont
+        font-size 22px
+        color #ffffff
+    .header_title
+      position absolute
+      top 50%
+      left 50%
+      transform translate(-50%, -50%)
+      width 30%
+      color #ffffff
+  .order_no_login
+    padding-top 140px
+    width 60%
+    margin 0 auto
+    text-align center
+    >img
+      display block
+      width 100%
+      height 30%
+    >h3
+      padding 10px 0
+      font-size 17px
+      color #6a6a6a
+    >button
+      display inline-block
+      background #02a774
+      font-size 14px
+      color #ffffff
+      border 0
+      outline none
+      border-radius 5px
+      padding 10px 20px
+</style>
---
+++ src/pages/Search/Search.vue
@@ -0,0 +1,56 @@
+<template>
+  <section class="search">
+    <header class="header">
+      <a class="header_title">
+        <span class="header_title_text">搜索</span>
+      </a>
+    </header>
+    <form action="#" class="search_form">
+      <input type="search" placeholder="请输入商家或美食名称" name="search">
+      <input type="submit" name="submit" class="search_submmit">
+    </form>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="stylus">
+.search
+  width 100%
+  .search_form
+    clearFix()
+    margin-top 45px
+    background-color #ffffff
+    padding 12px 8px
+    input
+      height 35px
+      padding 0 4px
+      border-radius 2px
+      font-weight bold
+      outline none
+      &.search_input
+        float left
+        width 79%
+        border 4px solid #f2f2f2
+        font-size 14px
+        color #333
+        background-color #f2f2f2
+      &.search_submmit
+        float right
+        width 18%
+        border 4px solid #02a774
+        font-size 16px
+        color #ffffff
+        background-color #02a774
+  .list
+    .list_container
+      background-color #ffffff
+  .search_none
+    margin 0 auto 
+    color #333
+    background-color #ffffff
+    text-align center
+    margin-top 0.125rem
+</style>
